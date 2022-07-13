aaf700840c2d2537ddba8f6cb43ed913c1bc21d5
Julio Cesar Almeida <julio.cesar@gat.digital>
2022-07-13 16:00:15 -0300

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"github.com/prometheus/common/log"
+	log "github.com/sirupsen/logrus"
 	"github.com/prometheus/common/version"
 	"github.com/shamil/burrow_exporter/exporter"
 	"gopkg.in/alecthomas/kingpin.v2"
